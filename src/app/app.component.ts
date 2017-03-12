34dd0780c0733c2e07449f04d39efdbd5847d630
Koz <koz4221@yahoo.com>
2017-03-12 18:00:52 -0400

    fd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'app works! t4';
-=======
   title = 'Eve ISK';
->>>>>>> ceff0d8d3a1d567e8c0895ee94e4d5a198181c65
 }
