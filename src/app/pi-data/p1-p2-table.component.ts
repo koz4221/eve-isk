3cfa45dcea50b418e6aa36924c73fc70b4aa7c8b
koz <koz4221@yahoo.com>
2017-04-04 16:49:05 -0400

    got css working, oh yeah

--- src/app/pi-data/p1-p2-table.component.ts
+++ src/app/pi-data/p1-p2-table.component.ts
@@ -8,7 +8,8 @@ import { PICalcService } from './pi-calc.service';
 @Component({
    moduleId: module.id,
    selector: 'p1-p2-table',
-   templateUrl: './p1-p2-table.component.html'
+   templateUrl: './p1-p2-table.component.html',
+   styleUrls: ['../css/pi-data.css']
 })
 
 export class P1toP2TableComponent implements OnInit {
@@ -23,8 +24,22 @@ export class P1toP2TableComponent implements OnInit {
       this.data = this.piDataService.data.filter(tid => tid.pClass === 2);
    }
 
-   ngOnChanges() {
-      console.log("on changeS");
+   public isTotalPositive(p: PIData): boolean {
+      if (p.jitaBuy != 0 && p.jitaSell != 0) {
+         if (+this.getP1toP2TotalProfit(p) > 0) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   isTotalNegative(p: PIData): boolean {
+      if (p.jitaBuy != 0 && p.jitaSell != 0) {
+         if (this.getP1toP2TotalProfit(p) < 0) {
+            return true;
+         }
+      }
+      return false;
    }
 
    // function solely to make the html {{}} not hideous
@@ -32,7 +47,7 @@ export class P1toP2TableComponent implements OnInit {
       return this.piDataService.getPIDataByTypeID(typeID)
    }
 
-   public getP1toP2TotalProfit(p: PIData): string {
+   public getP1toP2TotalProfit(p: PIData): number {
       let total: number = this.piCalcService.getP1toP2TotalProfit(
          this.piDataService.getPIDataByTypeID(p.input1).jitaSell,
          this.piDataService.getPIDataByTypeID(p.input2).jitaSell,
@@ -40,6 +55,6 @@ export class P1toP2TableComponent implements OnInit {
       )
       this.data.find(item => item.typeId == p.typeId).cssValue = String(total);
 
-      return this.piCalcService.formatNumberString(total);
+      return total;
    }
 }
