f66ced3fbb1120554a52e6108f85aa48d4aa10ea
koz <koz4221@yahoo.com>
2017-05-03 22:46:35 -0400

    added rest of doctrines

--- src/app/market/import-items.ts
+++ src/app/market/import-items.ts
@@ -43,5 +43,112 @@ export const IMPORT_ITEMS: any[] = [
          31790,       // Medium Core Defense Field Extender I
          4395         // Medium Processor Overclocking Unit I
       ]
+   },
+   {
+      group: "Maelstroms",
+      items: [
+         24694,       // Maelstrom
+         2048,        // Damage Control II
+         519,         // Gyrostabilizer II
+         1541,        // Power Diagnostic System II
+         12084,       // 100MN Microwarpdrive II
+         3841,        // Large Shield Extender II
+         2281,        // Adaptive Invulnerability Field II
+         2303,        // Thermic Dissipation Field II
+         2301,        // EM Ward Field II
+         2961,        // 1400mm Howitzer Artillery II
+         26088,       // Large Core Defense Field Extender I
+         28215,       // Bouncer II
+         21894,       // Republic Fleet EMP L
+         21918,       // Republic Fleet Phased Plasma L
+         21902,       // Republic Fleet Fusion L
+         21910,       // Republic Fleet Nuclear L
+         21926,       // Republic Fleet Proton L
+         28332,       // Republic Fleet Depleted Uranium L
+         28668,       // Nanite Repair Paste
+         9491,        // 1400mm 'Scout' Artillery I
+         2929,        // 800mm Repeating Cannon II
+         12775,       // Barrage L
+         9327,        // 800mm Heavy 'Scout' Repeating Cannon I
+      ]
+   },
+   {
+      group: "ANIs",
+      items: [
+         29337,       // Augoror Navy Issue
+         2048,        // Damage Control II
+         20353,       // 1600mm Reinforced Steel Plates II
+         11648,       // Armor Thermic Hardener II
+         11644,       // Armor Kinetic Hardener II
+         11646,       // Armor Explosive Hardener II
+         2364,        // Heat Sink II
+         3568,        // Small Capacitor Booster II
+         3025,        // Heavy Beam Laser II
+         6963,        // Heavy Modulated Energy Beam I
+         31055,       // Medium Trimark Armor Pump I
+         30999,       // Medium Anti-EM Pump I
+         12822,       // Aurora M
+         23089,       // Imperial Navy Multifrequency M
+         23097,       // Imperial Navy Standard M
+         28668,       // Nanite Repair Paste
+         32006,       // Navy Cap Booster 400
+      ]
+   },
+   {
+      group: "Guardians",
+      items: [
+         11987,       // Guardian
+         20353,       // 1600mm Reinforced Steel Plates II
+         2048,        // Damage Control II
+         11269,       // Energized Adaptive Nano Membrane II
+         11648,       // Armor Thermic Hardener II
+         11642,       // Armor EM Hardener II
+         12058,       // 10MN Afterburner II
+         16487,       // Large 'Regard' Remote Capacitor Transmitter
+         16451,       // Large Coaxial Remote Armor Repairer
+         26913,       // Medium Remote Armor Repairer II
+         31055,       // Medium Trimark Armor Pump I
+      ]
+   },
+   {
+      group: "Caracals",
+      items: [
+         621,         // Caracal
+         2048,        // Damage Control II
+         22291,       // Ballistic Control System II
+         3841,        // Large Shield Extender II
+         2281,        // Adaptive Invulnerability Field II
+         1877,        // Rapid Light Missile Launcher II
+         31790,       // Medium Core Defense Field Extender I
+         31600,       // Medium Hydraulic Bay Thrusters I
+         27361,       // Caldari Navy Scourge Light Missile
+         27371,       // Caldari Navy Inferno Light Missile
+         27381,       // Caldari Navy Nova Light Missile
+         27387,       // Caldari Navy Mjolnir Light Missile
+         28668,       // Nanite Repair Paste
+         8027,        // Prototype 'Arbalest' Rapid Light Missile Launcher
+      ]
+   },
+   {
+      group: "Jackdaws",
+      items: [
+         34828,       // Jackdaw
+         1541,        // Power Diagnostic System II
+         22291,       // Ballistic Control System II
+         31928,       // Republic Fleet Medium Shield Extender
+         2281,        // Adaptive Invulnerability Field II
+         2553,        // EM Ward Amplifier II
+         2404,        // Light Missile Launcher II
+         26931,       // Small Processor Overclocking Unit II
+         31788,       // Small Core Defense Field Extender I
+         27361,       // Caldari Navy Scourge Light Missile
+         27371,       // Caldari Navy Inferno Light Missile
+         27381,       // Caldari Navy Nova Light Missile
+         27387,       // Caldari Navy Mjolnir Light Missile
+         24495,       // Scourge Fury Light Missile
+         24497,       // Nova Fury Light Missile
+         24499,       // Inferno Fury Light Missile
+         2613,        // Mjolnir Fury Light Missile
+      ]
    }
 ]
